ea7c3833c2962508bb40f0c6853d8eddd75ae2ab
AzfarHanif <azfarhanifamulya@gmail.com>
2025-05-06 23:36:29 +0700

    js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
+const icon = document.getElementById('toggle-icon');
+
 document.getElementById('toggle-dark-mode').addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
-    const nav = document.getElementById('mainNavbar');
-    nav.classList.toggle('navbar-light');
-    nav.classList.toggle('navbar-dark');
-    nav.classList.toggle('bg-light');
-    nav.classList.toggle('bg-dark');
-  });
+  document.body.classList.toggle('dark-mode');
+  const nav = document.getElementById('mainNavbar');
+  nav.classList.toggle('navbar-light');
+  nav.classList.toggle('navbar-dark');
+  nav.classList.toggle('bg-light');
+  nav.classList.toggle('bg-dark');
+
+  // Ganti gambar ikon
+  if (document.body.classList.contains('dark-mode')) {
+    icon.src = 'assets/icons/dark-mode.png';
+  } else {
+    icon.src = 'assets/icons/light_mode.png';
+  }
+});
