9a276cc52515c1746ed51397235939e08d72b826
AzfarHanif <azfarhanifamulya@gmail.com>
2025-05-06 23:06:57 +0700

    hmm

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 document.getElementById('toggle-dark-mode').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
+    const nav = document.getElementById('mainNavbar');
+    nav.classList.toggle('navbar-light');
+    nav.classList.toggle('navbar-dark');
+    nav.classList.toggle('bg-light');
+    nav.classList.toggle('bg-dark');
   });
-  
